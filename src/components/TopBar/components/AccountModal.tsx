12fcf7a968f4a6aec4856a57320515c02d025b7d
ANNE13200 <64467420+ANNE13200@users.noreply.github.com>
2021-07-01 17:22:46 +0200

    Update AccountModal.tsx

--- src/components/TopBar/components/AccountModal.tsx
+++ src/components/TopBar/components/AccountModal.tsx
@@ -35,7 +35,7 @@ const AccountModal: React.FC<ModalProps> = ({ onDismiss }) => {
         <div style={{ display: 'flex' }}>
           <StyledBalanceWrapper>
             <CardIcon>
-              <span>⚖️</span>
+              <span>🏆</span>
             </CardIcon>
             <StyledBalance>
               <Value value={getBalanceNumber(welbBalance)} />
