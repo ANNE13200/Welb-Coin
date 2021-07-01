55db3d1144d81f18f94dc93b58ac6bbab9429f8b
ANNE13200 <64467420+ANNE13200@users.noreply.github.com>
2021-07-01 17:24:58 +0200

    Update Harvest.tsx

--- src/views/Farm/components/Harvest.tsx
+++ src/views/Farm/components/Harvest.tsx
@@ -24,7 +24,7 @@ const Harvest: React.FC<HarvestProps> = ({ pid }) => {
       <CardContent>
         <StyledCardContentInner>
           <StyledCardHeader>
-            <CardIcon>⚖️</CardIcon>
+            <CardIcon>🏆</CardIcon>
             <Value value={getBalanceNumber(earnings)} />
             <Label text="WELB Earned" />
           </StyledCardHeader>
